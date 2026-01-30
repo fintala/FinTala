4017eca05de9124d3e943f71b330ed76a6e60a93
fintala <fintala1012@gmail.com>
2026-01-30 12:30:25 +0200

    update

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,8 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache =>
       cache.addAll([
-        "./"
+        "./",
+        "./index.html"
       ])
     )
   );
