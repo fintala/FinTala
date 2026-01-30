76138030e0ca640bca9a8cc287a7070c20de15d2
fintala <fintala1012@gmail.com>
2026-01-30 08:36:43 +0200

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,12 @@
+const CACHE_NAME = "v0.0.4";
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("fintala-v0.0.4").then(cache =>
+    caches.open(CACHE_NAME).then(cache =>
       cache.addAll([
         "./",
         "./index.html",
-        "./css/dashboard.css",
-        "./css/main.css"
+
       ])
     )
   );
@@ -15,6 +16,18 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response =>
       response || fetch(event.request)
+    )
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
     )
   );
 });
