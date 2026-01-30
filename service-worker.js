0eb6e78087f07bfbbdd228a7608dc2b59ac36435
fintala <fintala1012@gmail.com>
2026-01-30 13:32:05 +0200

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "v0.0.0.0.5";
+const CACHE_NAME = "v0.0.0.0.6";
 
 self.addEventListener("activate", event => {
   event.waitUntil(
@@ -16,8 +16,7 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache =>
       cache.addAll([
-        "./",
-        "./index.html"
+        "./"
       ])
     )
   );
