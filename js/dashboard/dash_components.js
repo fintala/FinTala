d339c809bfb3966ddb4a65762a7c3e5f1a422323
fintala <fintala1012@gmail.com>
2026-02-07 10:39:45 +0200

    update

--- js/dashboard/dash_components.js
+++ js/dashboard/dash_components.js
@@ -42,7 +42,11 @@ Promise.all(pull.map(file => d3.json(file)))
     const width = 335 - margin.left - margin.right;
     const height = 305 - margin.top - margin.bottom;
     
-    const svg = d3.select('#vol-container')
+    const svg = d3.select('#vol-container');
+    
+    svg.selectAll('*').remove();
+    
+    svg
       .append('svg')
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
