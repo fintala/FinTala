9b9f1e3a5798ad82feb351110bedb1ca10fccded
fintala <fintala1012@gmail.com>
2026-02-07 12:19:09 +0200

    update

--- js/dashboard/dash_components.js
+++ js/dashboard/dash_components.js
@@ -7,10 +7,10 @@ let pull = [];
 let volTime = "";
 
 function pushSource() {
-  if (volTime === "W1") {
+  if (volTime == "W1") {
     pull = wFiles;
   }
-  else if (volTime === "Y1") {
+  else if (volTime == "Y1") {
     pull = 0;
   }
   else {
