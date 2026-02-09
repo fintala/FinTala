f06fc54cc5f2943aa34a73cd959d6f827dcb4272
fintala <fintala1012@gmail.com>
2026-02-10 01:37:59 +0200

    update

--- js/dashboard/dash_components.js
+++ js/dashboard/dash_components.js
@@ -217,7 +217,7 @@ Promise.allSettled(indexTimeframe).then((results) => {
       
     let barWidth = x.bandwidth();
     
-    const tickValues = d3.range(minValue, maxValue + 250000, 600000);
+    const tickValues = d3.range(0, d => d.masi + 250000, 600000);
       
     masiSvg.append('g')
       .attr('transform', `translate(${wIdth + 20}, 0)`)
