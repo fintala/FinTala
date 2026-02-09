42afbf9d31c7c07f340f2f0c5eb1489f8bdac81d
fintala <fintala1012@gmail.com>
2026-02-09 22:55:33 +0200

    update

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Stable, update-safe, PWA-friendly
 // ===============================
 
-const CACHE_NAME = 'v0.0.5.9.4';
+const CACHE_NAME = 'v0.0.5.9.5';
 
 // -------------------------------
 // INSTALL
