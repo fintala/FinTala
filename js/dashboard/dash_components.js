78a4b3b78de96a74b1761f4784a26d0605181638
fintala <fintala1012@gmail.com>
2026-02-09 20:35:54 +0200

    update

--- js/dashboard/dash_components.js
+++ js/dashboard/dash_components.js
@@ -236,7 +236,7 @@ Promise.allSettled(indexTimeframe).then((results) => {
       .attr("height", d => y(0) + y(d.masi))
       .attr("fill", "magenta")
       .attr("stroke", "black")
-      .style('transform', 'translateY(-3rem)');
+      .style('transform', 'translateY(-6rem)');
   
 });
 
