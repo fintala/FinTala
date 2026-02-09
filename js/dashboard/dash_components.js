d291a956154a0d916a90e97fa9290a001c85bd7f
fintala <fintala1012@gmail.com>
2026-02-09 19:14:32 +0200

    update

--- js/dashboard/dash_components.js
+++ js/dashboard/dash_components.js
@@ -171,7 +171,7 @@ Promise.allSettled(indexTimeframe).then((results) => {
   
   const edge = { top: 20, right: 60, bottom: 10, left: 30 };
   const wIdth = 330 - edge.left - edge.right;
-  const hEight = 120 - edge.top - edge.bottom;
+  const hEight = 125 - edge.top - edge.bottom;
   
   let startIndex = 0;
   
