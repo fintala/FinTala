ac8994d2e687b54d3997405be49e5509bc9c316c
fintala <fintala1012@gmail.com>
2026-02-10 01:12:58 +0200

    update

--- js/dashboard/dash_components.js
+++ js/dashboard/dash_components.js
@@ -210,7 +210,7 @@ Promise.allSettled(indexTimeframe).then((results) => {
     const y = d3.scalePow()
       .exponent(5)
       .domain([0, d3.max
-      (visibleData, d => d.masi) * 1.1])
+      (visibleData, d => d.masi)])
       .range([hEight, -2]);
       
       console.log(indexData.map(d => d.masi));
