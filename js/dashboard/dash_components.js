d74bf3988fe081f1bc725f4408ca6d645f6008b1
fintala <fintala1012@gmail.com>
2026-02-09 13:32:23 +0200

    update

--- js/dashboard/dash_components.js
+++ js/dashboard/dash_components.js
@@ -25,7 +25,7 @@ Promise.allSettled(indexTimeframe).then((results) => {
   const currentPoint = indexData[indexData.length - 1].masi;
   const previousPoint = indexData[indexData.length - 2].masi;
   
-  const percentageChange = (((currentPoint - previousPoint)/(currentPoint))*100).toFixed(2);
+  const percentageChange = (((currentPoint - previousPoint- 100000)/(currentPoint))*100).toFixed(2);
   
   console.log(percentageChange);
     
