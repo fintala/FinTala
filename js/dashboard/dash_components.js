e3288d9dae2e497f37b41e8815f808f2954582df
fintala <fintala1012@gmail.com>
2026-02-09 20:05:54 +0200

    update

--- js/dashboard/dash_components.js
+++ js/dashboard/dash_components.js
@@ -233,7 +233,7 @@ Promise.allSettled(indexTimeframe).then((results) => {
       .attr("x", d => x(d.date)) // adjust x to center the bar
       .attr("y", d => y(0))
       .attr("width", barWidth)
-      .attr("height", d => y(0) + y(masi))
+      .attr("height", d => y(0) + y(d.masi))
       .attr("fill", "magenta")
       .attr("stroke", "black");
   
