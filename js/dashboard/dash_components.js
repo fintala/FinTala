0a80e217080cd6b9cef7baef2fd585d668343cb6
fintala <fintala1012@gmail.com>
2026-02-12 13:37:45 +0200

    update

--- js/dashboard/dash_components.js
+++ js/dashboard/dash_components.js
@@ -401,8 +401,8 @@ Promise.allSettled(indexTimeframe).then((results) => {
   renderThreshold();
     
   masiBars.on("click", (event, d) => {
-    
-    console.log(d);
+    singleBarIndex = dataArray[0].indexOf(d);
+    console.log(singleBarIndex);
     // update currentBar
     const currentBar = indexData.slice(singleBarIndex, singleBarIndex + 1);
     // update your display/logic here
