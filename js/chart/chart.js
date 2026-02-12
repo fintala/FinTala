026a41b4e8dc531d6db74291fb4946c08377f47d
fintala <fintala1012@gmail.com>
2026-02-12 07:21:11 +0200

    update

--- js/chart/chart.js
+++ js/chart/chart.js
@@ -497,7 +497,7 @@ function render() {
       .attr("stroke", "black");
       
       // appending close price tag
-      overlayLayer.append("line")
+      overlay.append("line")
       .data(visibleData)
       .attr("x1", 0)
       .attr("x2", width + 10)
