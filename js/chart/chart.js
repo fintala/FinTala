f6ee5f2e06c49f86e14f26e777cd16b0d8492992
fintala <fintala1012@gmail.com>
2026-02-12 09:14:42 +0200

    update

--- js/chart/chart.js
+++ js/chart/chart.js
@@ -531,14 +531,14 @@ function render() {
     .style("opacity", "0.8");
     
   axisLayer.append("rect")
-    .attr("x", width + 1) // position it a bit to the right of the chart
+    .attr("x", 1) // position it a bit to the right of the chart
     .attr("y", y(d3.min(currentCandle, d => d.close)) - 10)
     .attr("width", margin.right - 2)
     .attr("height", 20)
     .attr("fill", "#c1ff72");
   
   axisLayer.append("text")
-    .attr("x", width + 3)
+    .attr("x", 3)
     .attr("y", y(d3.min(currentCandle, d => d.close)) + 5)
     .text(y(d3.min(currentCandle, d => d.close)))
     .attr("fill", "black")
--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Stable, update-safe, PWA-friendly
 // ===============================
 
-const CACHE_NAME = 'v0.0.7.2.6';
+const CACHE_NAME = 'v0.0.7.2.7';
 
 // -------------------------------
 // INSTALL
