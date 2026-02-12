a171187d7f4f6d464ae4a2f1ec3c11e03eaac956
fintala <fintala1012@gmail.com>
2026-02-12 07:57:26 +0200

    update

--- js/chart/chart.js
+++ js/chart/chart.js
@@ -497,13 +497,12 @@ function render() {
       .attr("fill", d => d.close >= d.open ? "#c1ff72" : "#2c3e50")
       .attr("stroke", "black");
       
-      // appending close price tag
-      overlayLayer.append("line")
-      .data(currentCandle)
+    // appending close price tag
+    overlayLayer.append("line")
       .attr("x1", 0)
       .attr("x2", width + 10)
-      .attr("y1", d => Math.abs(1, y(d.close)))
-      .attr("y2", d => Math.abs(1, y(d.close)))
+      .attr("y1", y(d3.min(currentCandle, d => d.close)))
+      .attr("y2", y(d3.min(currentCandle, d => d.close)))
       .attr("stroke", "#660033")
       .attr("stroke-dasharray", "4 2")
       .style("opacity", "0.7");
